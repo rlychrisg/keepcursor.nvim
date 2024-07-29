7d542667a5fa9352195557484e3db6d966bfdc2f
Chris G <howmuchiswhere@gmail.com>
2024-07-29 07:16:31 +0100

    one var to track instead of three

--- lua/keepcursor/init.lua
+++ lua/keepcursor/init.lua
@@ -1,21 +1,19 @@
 local M = {}
 
 function M.ToggleCursorTop(newscrolloff)
-    if _G.CursorTopEnabled then
+    if _G.KeepCursorAt == "top" then
         -- disable previous autocmd
         vim.cmd[[autocmd! KeepCursor]] -- doing this in lua throws errors sometimes so for now sticking with vimscript
         -- variable to track state
-        _G.CursorTopEnabled = false
+        _G.KeepCursorAt = 'off'
         -- return to previous value
         vim.opt.scrolloff = _G.prev_scrolloff
         print("KeepCursor: Scrolloff returned to default.")
     else
         -- if the other one is enabled
-        if _G.CursorBotEnabled or _G.CursorMidEnabled then
+        if _G.KeepCursorAt == "middle" or _G.KeepCursorAt == "bottom" then
             vim.cmd[[autocmd! KeepCursor]]
-            _G.CursorBotEnabled = false
-            _G.CursorMidEnabled = false
-            _G.CursorTopEnabled = true
+            _G.KeepCursorAt = "top" -- TODO i might be able to delete this line since var is set later
         else
             -- if none of them are, get the scrolloff for later
             _G.prev_scrolloff = vim.opt.scrolloff:get()
@@ -28,24 +26,22 @@ function M.ToggleCursorTop(newscrolloff)
         vim.api.nvim_create_augroup("KeepCursor", { clear = true })
         vim.api.nvim_create_autocmd("CursorMoved", { command = "normal! zt", group = "KeepCursor" })
 
-        _G.CursorTopEnabled = true
+        _G.KeepCursorAt = "top"
         print("KeepCursor: keeping cursor at top.")
     end
 end
 
 -- follows the previous function's format
 function M.ToggleCursorBot(newscrolloff)
-    if _G.CursorBotEnabled then
+    if _G.KeepCursorAt == "bottom" then
         vim.cmd[[autocmd! KeepCursor]]
-        _G.CursorBotEnabled = false
+        _G.KeepCursorAt = "off"
         vim.opt.scrolloff = _G.prev_scrolloff
         print("KeepCursor: Scrolloff returned to default.")
     else
-        if _G.CursorTopEnabled or _G.CursorMidEnabled then
+        if _G.KeepCursorAt == "top" or _G.KeepCursorAt == "middle" then
             vim.cmd[[autocmd! KeepCursor]]
-            _G.CursorTopEnabled = false
-            _G.CursorMidEnabled = false
-            _G.CursorBotEnabled = true
+            _G.KeepCursorAt = "bottom" -- TODO again may not need
         else
             _G.prev_scrolloff = vim.opt.scrolloff:get()
         end
@@ -54,30 +50,28 @@ function M.ToggleCursorBot(newscrolloff)
         end
         vim.api.nvim_create_augroup("KeepCursor", { clear = true })
         vim.api.nvim_create_autocmd("CursorMoved", { command = "normal! zb", group = "KeepCursor" })
-        _G.CursorBotEnabled = true
+        _G.KeepCursorAt = "bottom"
         print("KeepCursor: keeping cursor at bottom.")
     end
 end
 
 -- and again, only without the new scroll off
 function M.ToggleCursorMid()
-    if _G.CursorMidEnabled then
+    if _G.KeepCursorAt == "middle" then
         vim.cmd[[autocmd! KeepCursor]]
-        _G.CursorMidEnabled = false
+        _G.KeepCursorAt = "off"
         vim.opt.scrolloff = _G.prev_scrolloff
         print("KeepCursor: Scrolloff returned to default.")
     else
-        if _G.CursorTopEnabled or _G.CursorBotEnabled then
+        if _G.KeepCursorAt == "top" or _G.KeepCursorAt == "bottom" then
             vim.cmd[[autocmd! KeepCursor]]
-            _G.CursorTopEnabled = false
-            _G.CursorBotEnabled = false
-            _G.CursorMidEnabled = true
+            _G.KeepCursorAt = "middle" -- and again
         else
             _G.prev_scrolloff = vim.opt.scrolloff:get()
         end
         vim.api.nvim_create_augroup("KeepCursor", { clear = true })
         vim.api.nvim_create_autocmd("CursorMoved", { command = "normal! zz", group = "KeepCursor" })
-        _G.CursorMidEnabled = true
+        _G.KeepCursorAt = "middle"
         print("KeepCursor: keeping cursor at middle.")
     end
 end
@@ -85,11 +79,9 @@ end
 
 -- disable any cursor autocmd functions
 function M.DisableKeepCursor()
-    if _G.CursorBotEnabled or _G.CursorTopEnabled or _G.CursorMidEnabled then
+    if _G.KeepCursorAt ~= "off" then
         vim.cmd[[autocmd! KeepCursor]]
-        _G.CursorTopEnabled = false
-        _G.CursorMidEnabled = false
-        _G.CursorBotEnabled = false
+        _G.KeepCursorAt = "off"
         -- and return scroll off to default
         vim.opt.scrolloff = _G.prev_scrolloff
         print("KeepCursor: Scrolloff returned to default.")
